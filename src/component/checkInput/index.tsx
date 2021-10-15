7cee2ddad584cf91d8bdf48cdd42d8da5cb5ecf4
IrvingZhao <irving.zhao.dev@hotmail.com>
2021-10-15 10:06:54 +0800

    feat(components): 新增可开关的input

---
+++ src/component/checkInput/index.tsx
@@ -0,0 +1,78 @@
+import { defineComponent, PropType, inject, computed, ref, watch } from 'vue'
+import { elFormKey, elFormItemKey, ElFormContext, ElFormItemContext, ElInput, ElCheckbox } from 'element-plus'
+import './style.scss'
+
+export interface CheckInputValue {
+  text: string
+  check: string
+}
+
+export default defineComponent({
+  name: 'PjCheckInput',
+  inheritAttrs: false,
+  components: { ElInput, ElCheckbox },
+  props: {
+    modelValue: {
+      type: Object as PropType<CheckInputValue>,
+      required: true
+    },
+    checkLabel: {
+      type: String,
+      default: ''
+    },
+    trueLabel: {
+      type: String,
+      default: 'true'
+    },
+    falseLabel: {
+      type: String,
+      default: 'false'
+    },
+    checkDisableInput: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  render(ctx, cache, props) {
+    return (
+      <div class={'pj-check-input'}>
+        <div class={'input-area'}>
+          <ElInput v-model={ctx.value.text} disabled={ctx.formItemDisable || ctx.inputDisable} />
+        </div>
+        <div class={'checkbox-area'}>
+          <ElCheckbox v-model={ctx.value.check} true-label={props.trueLabel} false-label={props.falseLabel}>
+            {props.checkLabel}
+          </ElCheckbox>
+        </div>
+      </div>
+    )
+  },
+  setup(props) {
+    const elForm = inject(elFormKey, {} as ElFormContext)
+    const elFormItem = inject(elFormItemKey, {} as ElFormItemContext)
+
+    const formItemDisable = computed(() => {
+      return props.disabled || elForm.disabled
+    })
+
+    const { value } = ref(props.modelValue)
+
+    const inputDisable = computed(() => {
+      return props.checkDisableInput && props.modelValue.check !== props.trueLabel
+    })
+
+    watch(value, () => {
+      elFormItem?.validate?.('change')
+    })
+
+    return {
+      formItemDisable,
+      inputDisable,
+      value
+    }
+  }
+})
--- src/component/index.ts
+++ src/component/index.ts
@@ -1,10 +1,12 @@
 import { App } from 'vue'
 import Button, { ButtonConfig } from './button'
+import CheckInput, { CheckInputValue } from './checkInput'
 
 export default {
   install(app: App) {
     app.component('pj-button', Button)
+    app.component('pj-check-input', CheckInput)
   }
 }
 
-export { ButtonConfig }
+export { ButtonConfig, CheckInputValue }
