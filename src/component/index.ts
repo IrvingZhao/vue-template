eaacefaa8a509ae8b77a622fe8e26cce1222cd09
IrvingZhao <irving.zhao.dev@hotmail.com>
2021-10-15 10:04:33 +0800

    feat(components): 新增支持config的按钮

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,3 +1,10 @@
+import { App } from 'vue'
+import Button, { ButtonConfig } from './button'
+
 export default {
-  install() {}
+  install(app: App) {
+    app.component('pj-button', Button)
+  }
 }
+
+export { ButtonConfig }
