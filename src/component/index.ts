055f51f46bfab203171a755959b95c4c34bc8e86
IrvingZhao <irving.zhao.dev@hotmail.com>
2021-10-19 11:33:19 +0800

    feat(components): 分页组件，添加pageInfo对象的支持

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,11 +1,13 @@
 import { App } from 'vue'
 import Button from './button'
 import CheckInput from './checkInput'
+import Pagination from './pagination'
 
 export default {
   install(app: App) {
     app.component('pj-button', Button)
     app.component('pj-check-input', CheckInput)
+    app.component('pj-pagination', Pagination)
   }
 }
 
