ae621d3698cff1a2ffb09cfedfb4371f0349694d
IrvingZhao <irving.zhao.dev@hotmail.com>
2021-10-21 17:18:48 +0800

    feat(components): 新增密码框

--- src/component/index.ts
+++ src/component/index.ts
@@ -4,6 +4,7 @@ import CheckInput from './checkInput'
 import Dialog from './dialog/index.vue'
 import MultipleSelect from './multiple-select'
 import Pagination from './pagination'
+import Password from './password'
 
 export default {
   install(app: App) {
@@ -12,10 +13,11 @@ export default {
     app.component('pj-dialog', Dialog)
     app.component('pj-multiple-select', MultipleSelect)
     app.component('pj-pagination', Pagination)
+    app.component('pj-password', Password)
   }
 }
 
-export { Button, CheckInput, Dialog, Pagination }
+export { Button, CheckInput, Dialog, Pagination, Password }
 
 export * from './button'
 export * from './checkInput'
