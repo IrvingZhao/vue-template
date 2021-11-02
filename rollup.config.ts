6d50a5903ea1b1a8f61ee476d079f9e529edc54b
IrvingZhao <irving.zhao.dev@hotmail.com>
2021-11-02 11:37:31 +0800

    feat(components): 新增 electron 依赖配置

---
+++ rollup.config.ts
@@ -0,0 +1,56 @@
+// 编译 electron index 和 preload 使用
+import commonjs from '@rollup/plugin-commonjs'
+import typescriptPlugin from '@rollup/plugin-typescript'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import copy from 'rollup-plugin-copy'
+
+const plugins = [
+  typescriptPlugin({
+    tsconfig: false,
+    include: ['electron/**/*.ts'],
+    target: 'esnext',
+    module: 'esnext',
+    strict: true,
+    importHelpers: true,
+    moduleResolution: 'node',
+    experimentalDecorators: true,
+    skipLibCheck: true,
+    esModuleInterop: true,
+    allowSyntheticDefaultImports: true,
+    sourceMap: false,
+    baseUrl: '.',
+    noImplicitAny: false,
+    resolveJsonModule: true
+  }),
+  commonjs(),
+  nodeResolve(),
+  copy({
+    targets: [
+      { src: 'env', dest: 'dist-electron' },
+      { src: 'electron/devtools', dest: 'dist-electron' }
+    ]
+  })
+]
+
+const external = ['electron', 'child_process', 'fs', 'path', 'url', 'module', 'os']
+
+export default [
+  {
+    input: 'electron/index.ts',
+    output: {
+      file: 'dist-electron/index.js',
+      format: 'cjs'
+    },
+    plugins,
+    external
+  },
+  {
+    input: 'electron/preload.ts',
+    output: {
+      file: 'dist-electron/preload.js',
+      format: 'cjs'
+    },
+    plugins,
+    external
+  }
+]
