6486b90a72727db748b06973c5087453c6421501
IrvingZhao <irving.zhao.dev@hotmail.com>
2021-11-30 11:23:59 +0800

    feat(components): 添加worker

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,6 @@
 // 编译 electron index 和 preload 使用
+import { readdirSync } from 'fs'
+import { resolve } from 'path'
 import commonjs from '@rollup/plugin-commonjs'
 import typescriptPlugin from '@rollup/plugin-typescript'
 import nodeResolve from '@rollup/plugin-node-resolve'
@@ -32,9 +34,27 @@ const plugins = [
   })
 ]
 
-const external = ['electron', 'child_process', 'fs', 'path', 'url', 'module', 'os']
+const external = ['electron']
+
+const workerPath = 'electron/worker'
+const workerDir = resolve(__dirname, workerPath)
+const workerFiles = readdirSync(workerDir)
+const workerBuildConfig = workerFiles
+  .filter((item) => item !== 'index.ts')
+  .map((item) => {
+    return {
+      input: `${workerPath}/${item}`,
+      output: {
+        file: `dist-electron/worker/${item}.js`,
+        format: 'cjs'
+      },
+      plugins,
+      external
+    }
+  })
 
 export default [
+  ...workerBuildConfig,
   {
     input: 'electron/index.ts',
     output: {
